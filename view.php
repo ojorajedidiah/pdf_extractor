31e1646bbeee2fcca11a2cdf364ff7c0d3e73ae2
jedidiah <ojorajedidiah@gmail.com>
2024-01-26 13:20:44 +0100

    update to view records ==> view details

--- view.php
+++ view.php
@@ -24,11 +24,28 @@ include('classes/databaseConnection.class.php');
 </head>
 
 <body class="hold-transition layout-top-nav">
-  <!-- <div id="app"> -->
-    <div class="wrapper">
-      <?php include('assets/includes/top_menu.php'); ?>
-      <div class="content-wrapper">
-        <div class="content">
+  <div class="wrapper">
+    <?php include('assets/includes/top_menu.php'); ?>
+    <div class="content-wrapper">
+      <div class="content">
+
+        <?php if (isset($_REQUEST['rid'])) { ?>
+          <div class="container-fluid" style="width:45%;">
+            <div class="card card-outline card-success">
+              <div class="card-header">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <h5>CBN Collections</h5>
+                  </div>
+                  <div class="col-sm-4">
+                    <a href="view.php" class="btn btn-secondary float-right">Back</a>
+                  </div>
+                </div>
+              </div>
+          <div class="card-body">
+            <?php echo getSpecificRecord($_REQUEST['rid']); ?>
+          </div>
+        <?php } else { ?>
           <div class="container-fluid" style="width:85%;">
             <div class="card card-outline card-success">
               <div class="card-header">
@@ -41,56 +58,59 @@ include('classes/databaseConnection.class.php');
                   </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="card-body">
-                  <table id="grids" class="table table-bordered table-striped">
-                    <thead>
-                      <tr>
-                        <th></th>
-                        <th>Account Number</th>
-                        <th>Report Date</th>
-                        <th>Statement Date</th>
-                        <th>Opening Balance</th>
-                        <th>Closing Balance</th>
-                        <th>Debit(s)</th>
-                        <th>Collection</th>                        
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php echo getCollectionRecords(); ?>
-                    </tbody>
-                  </table>
+              <div class="card-body">
+                <div class="row">
+                  <div class="card-body">
+                    <table id="grids" class="table table-bordered table-striped">
+                      <thead>
+                        <tr>
+                          <th></th>
+                          <th>Account Number</th>
+                          <th>Report Date</th>
+                          <th>Statement Date</th>
+                          <th>Opening Balance</th>
+                          <th>Closing Balance</th>
+                          <th>Debit(s)</th>
+                          <th>Collection</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php echo getCollectionRecords(); ?>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        <?php } ?>
       </div>
-    <!-- </div> -->
+    </div>
   </div>
-  
+
 
   <script src="assets/datatables/jquery.dataTables.min.js"></script>
   <script src="assets/datatables/dataTables.bootstrap4.min.js"></script>
   <script src="assets/datatables/dataTables.buttons.min.js"></script>
 
   <script>
-  $(function() {
-    $("#grids").DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "ordering": true,
-      "searching": true,
-      "info": true,
-      "autoWidth": true,
-      "responsive": true,
-      // "buttons": ["excel", "pdf", "colvis"]
-    }).buttons().container().appendTo('#grids_wrapper .col-md-6:eq(0)');
-  });
-</script>
+    $(function() {
+      $("#grids").DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "ordering": true,
+        "searching": true,
+        "info": true,
+        "autoWidth": true,
+        "responsive": true,
+        // "buttons": ["excel", "pdf", "colvis"]
+      }).buttons().container().appendTo('#grids_wrapper .col-md-6:eq(0)');
+    });
+  </script>
 
 </body>
-<?php //}   ?>
+<?php //}   
+?>
 
 <?php
 
@@ -111,7 +131,7 @@ function getCollectionRecords()
         $rID = $row['cid'];
         $diff = (floatval($row['cbal']) + floatval($row['debtot'])) - floatval($row['obal']);
 
-        $rtn .= '<tr><td><a href="view.php?rid=' . $rID . '"><i class="fas fa-money-check" title="Details" style="color:red;"></i></a></td><td>' 
+        $rtn .= '<tr><td><a href="view.php?rid=' . $rID . '"><i class="fas fa-money-check" title="Details" style="color:red;"></i></a></td><td>'
           . $row['can'] . '</td><td>' . $row['crd'] . '</td>'
           . '<td>' . $row['cad'] . '</td><td style="text-align: right;">' . number_format($row['obal'], 2) . '</td>'
           . '<td style="text-align: right;">' . number_format($row['cbal'], 2)
@@ -126,4 +146,52 @@ function getCollectionRecords()
     trigger_error($e->getMessage(), E_USER_NOTICE);
   }
   return ($rtn == '') ? '<tr><td colspan="7" style="color:red;text-align:center;"><b>No Collections Record Available</b></td></tr>' : $rtn;
+}
+
+
+function getSpecificRecord($rid)
+{
+  $rtn = '';
+  try {
+    $db = new connectDatabase();
+    if ($db->isLastQuerySuccessful()) {
+      $con = $db->connect();
+
+      $sql = "SELECT cid,crd,cad,can,obal,cbal,deb,debtot FROM cbn_collection_test WHERE cid=:rid";
+      $stmt = $con->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+      $stmt->bindparam(":rid", $rid, PDO::PARAM_INT);
+
+      $stmt->execute();
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+      foreach ($stmt->fetchAll() as $row) {
+        $rID = $row['cid'];
+        $diff = (floatval($row['cbal']) + floatval($row['debtot'])) - floatval($row['obal']);
+        $dt = new DateTime($row['cad']);
+
+        $rtn .= '<div class="row"><div class="col-sm-6"><div class="form-group">';
+        $rtn .= '<label for="can">Account Number </label><input type="text" class="form-control" name="can" readonly value="' . $row['can'] . '">';
+        $rtn .= '<label for="crd">Report Date and Time </label><input type="text" class="form-control" name="crd" readonly value="' . $row['crd'] . '">';
+        $rtn .= '<label for="cad">Account Date </label><input type="text" class="form-control" name="cad" readonly value="' . $dt->format('d M Y') . '">';
+        $rtn .= '</div></div>';
+
+        $rtn .= '<div class="col-sm-6"><div class="form-group">';
+        $rtn .= '<label for="obal">Opening Balance </label><input type="text" style="text-align: right;" class="form-control" name="obal" readonly value="' . number_format($row['obal'], 2) . '">';
+        $rtn .= '<label for="cbal">Closing Balance </label><input type="text" style="text-align: right;" class="form-control" name="cbal" readonly value="' . number_format($row['cbal'], 2) . '">';
+        $rtn .= '<label for="debtot">Debit Total </label><input type="text" style="text-align: right;" class="form-control" name="debtot" readonly value="' . number_format($row['debtot'], 2) . '">';
+        $rtn .= '</div></div>';
+
+        $rtn .= '<div class="col-sm-12"><div class="form-group">';
+        $rtn .= '<label for="deb">Debit Entry(s)</label>';
+        $rtn .= '<textarea class="form-control" rows="2" name="deb" readonly>' . $row['deb'] . '</textarea></div></div></div>';
+      }
+    } else {
+      trigger_error($db->connectionError(), E_USER_NOTICE);
+    }
+    $db->closeConnection();
+  } catch (Exception $e) {
+    trigger_error($e->getMessage(), E_USER_NOTICE);
+  }
+
+  return ($rtn == '') ? '<span style="color:red;text-align:center;"><b>No Collections Record for this ID</b></span></div>' : $rtn;
 }
