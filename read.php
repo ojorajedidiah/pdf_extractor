5bbde3cac0c15deedcf7d68a7a26dffe45d549ba
jedidiah <ojorajedidiah@gmail.com>
2024-01-28 19:50:20 +0100

    Update to handle 247s and 032s debit entries

--- read.php
+++ read.php
@@ -1,5 +1,24 @@
 <?php
 
+#****Statement Reader Bot v1.5
+#***Author of the script
+#***Name: Adeleke Ojora
+#***Email : adeleke.ojora@firs.gov.ng
+#***Date created: 11/01/2024
+#***Version number: v1.0
+#***Date modified: 22/01/2024
+#***Date modified by: Adeleke Ojora
+#***Version number: v1.25 Update with ability to understand protrait statements
+#***Date modified: 24/01/2024
+#***Date modified by: Adeleke Ojora
+#***Version number: v1.5  Update to enhance the functionality and incorporate business logic
+
+
+#   The code is to read the content of specific bank statement 
+#   to extract opening balance, closing balance and debit entry(s) 
+#   and other relevant information.
+
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/classes/databaseConnection.class.php';
 
@@ -11,7 +30,7 @@ if (isset($_POST['submit'])) {
     //$uploadFile = $uploadDir . basename($_FILES['pdfFile']['name']);
     $fileType = pathinfo($_FILES['pdfFile']['name'], PATHINFO_EXTENSION);
 
-    //Check if the file is a PDF
+    //************** Check if the uploaded file is a PDF ***************//
     if ($fileType != "pdf") {
         die('wrong file type!');
     } else {
@@ -22,13 +41,13 @@ if (isset($_POST['submit'])) {
             $text = Pdf::getText($dfile);
             $bala = getFigures($text);
 
-            //die(json_encode($bala));
-            if (saveToDatabase($bala)) {
-                $bala['message'] = 'The record ' . $bala['accountNumber'] . ' has been successfully saved!';
-            }
+            // if (saveToDatabase($bala)) {
+                $dtt=new DateTime($bala['accountDate']);
+                $bala['message'] = 'The record of <b>' . $bala['accountNumber'] .'</b> for <b>'. $dtt->format('d M Y'). '</b> has been successfully saved!';
+            // }
             presentExtracts($bala);
         } catch (Exception $e) {
-            die('there is an error ' . $e->getMessage());
+            die('there is an error <b>' . $e->getMessage().'</b>');
         }
     }
 }
@@ -47,7 +66,7 @@ function getFigures($str)
 
     $extracts = array();
 
-    // split the content by tabs
+    //*************** split the content by tabs ***************//
     $txt = str_split($str);
     $wrd = '';
     $words = array();
@@ -63,7 +82,7 @@ function getFigures($str)
         }
     }
 
-    // echo (json_encode($words));
+    // die(json_encode($words));
 
     if (
         in_array('Account Statement', $words)
@@ -71,16 +90,17 @@ function getFigures($str)
         || in_array('FEDERAL INLAND REVENUE SERV', $words)
         || in_array('ACCOUNTANT GEN OF THE FEDER', $words) 
         || in_array('Closing Balance', $words)
-        || in_array('Balance at Period E', $words)
+        || in_array('AT PERIOD', $words) 
+        || in_array('Balance at Period E', $words)  //'
     ) {
 
 
         echo 'Relevant content has been found and being processed...<br>';
 
-        // extract the reporting date/time
+        //*************** extract the reporting date/time ***************//
         $extracts['reportDate'] = trim($words[0]) . ' ' . trim($words[1]);
 
-        // extract the account number
+        //*************** extract the account number ***************//
         $acctNum = '';
         for ($i = 0; $i <= count($words) - 1; $i++) {
             if (substr($words[$i], 0, 2) == '00' && strlen($words[$i]) == 13) {
@@ -91,50 +111,62 @@ function getFigures($str)
         $extracts['accountNumber'] = $acctNum;
 
 
-        //extract the account date
+        //*************** extract the account date ***************//
         $acctDat = '';
         for ($i = 3; $i <= count($words) - 1; $i++) {
-            if (substr_count($words[$i],' ') > 1 && is_numeric(substr($words[$i],0,2))) {
-                $acctDat = $words[$i];
-                break;
+            if ($words[$i] == 'Value Date') { // get the first occurrance of Value Date
+                $nu=$i+1;
+                for($j=$nu; $j<=count($words); $j++) {
+                    if (substr_count($words[$j],' ') > 1    // if the word has more than one space
+                        && is_numeric(substr($words[$j],0,2))  // if the first two characters are numbers
+                        && strlen($words[$j]) == 9) {  // if the length of the word is 9 then it is a date
+                        $acctDat = $words[$j];
+                        break;
+                    }
+                }
             }
         }
+
+        // if the extracted date is empty then use the day before the report date
         if(strlen($acctDat)==0){
             $dt = new DateTime($words[0]);
             $intv = new DateInterval('P1D');
             $extracts['accountDate'] = $dt->sub($intv)->format('Y-m-d');
-        } else{
+        } else{ // other wise format the value
             $dt = new DateTime($acctDat);
             $extracts['accountDate'] = $dt->format('Y-m-d');
         }
 
-        //die(json_encode($extracts));
+        // die(json_encode($extracts));
 
-        // extract the opening balance
+        //*************** extract the opening balance ***************//
         $tm='';
         if (in_array('CBN STATEMENT OF ACCOUNT', $words)){ // strategy one
             for ($x = 0; $x < count($words); $x++) {
-                $hays = $words[$x];
-                $myit = $x + 10;
-                if (substr_count($hays, 'Balance At') >= 1) {  // extract strings that have Balance At in it
+                $hays = $words[$x];                
+                if ($hays == 'Balance At') {  // extract strings that have Balance At in it
                     // extract the next strings after the above but has commas (figures) 
                     // and does not contain the word Balance At
+                    // !(str_contains($words[$u], 'Balance At') && 
+                    $myit = $x + 10;
                     for ($u = $x + 1; $u <= $myit; $u++) {
-                        if (!(str_contains($words[$u], 'Balance At') && substr_count($words[$u], ',') >= 1)) {
+                        if (substr_count($words[$u], ',') >= 1) {
                             // this is the corresponding figure for the Balance identified above
                             $tm='';  //substr($e,$n,1) == '.' || substr($e,$n,1) == ',' || 
                             for($n=$u;$n<=$u+4;$n++){
-                                if(is_numeric(substr($words[$n],0,1))) {
+                                if((substr_count($words[$n], ',') >= 1 
+                                  || substr_count($words[$n], '.') >= 1) 
+                                  && is_numeric(substr($words[$n],0,1))) {
                                     $tm.= $words[$n];
                                 }
                             }                            
-                            break;
+                            break 2;
                         }
                     }
                 }
             }
-        } else { //if(in_array('ACCOUNTANT GEN OF THE FEDER', $words) 
-          //|| in_array('FEDERAL INLAND REVENUE SERV', $words)){    //strategy two
+        } else { //if(in_array('ACCOUNTANT GEN OF THE FEDER', $words) //strategy two
+          //|| in_array('FEDERAL INLAND REVENUE SERV', $words)){    
             for ($x = 0; $x < count($words); $x++) {
                 $hays = $words[$x];
                 $myit = $x + 4;
@@ -163,7 +195,8 @@ function getFigures($str)
 
         // die(json_encode($extracts));
 
-        // extract the closing balance
+
+        //********** extract the closing balance ***************//
         $tm='';
         if (in_array('CBN STATEMENT OF ACCOUNT', $words)){ // strategy one 
             for ($x = 0; $x < count($words); $x++) {
@@ -177,7 +210,9 @@ function getFigures($str)
                             // this is the corresponding figure for the Balance identified above
                             $tm='';  //substr($e,$n,1) == '.' || substr($e,$n,1) == ',' || 
                             for($n=$u;$n<=$u+3;$n++){
-                                if(is_numeric(substr($words[$n],0,1))) {
+                                if((substr_count($words[$n], ',') >= 1 
+                                  || substr_count($words[$n], '.') >= 1) 
+                                  && is_numeric(substr($words[$n],0,1))) {
                                     $tm.= $words[$n];
                                 }
                             }                            
@@ -186,8 +221,8 @@ function getFigures($str)
                     }
                 }
             }
-        } else  { //if(in_array('ACCOUNTANT GEN OF THE FEDER', $words) 
-          //|| in_array('FEDERAL INLAND REVENUE SERV', $words)){    //strategy two 
+        } else  { //if(in_array('ACCOUNTANT GEN OF THE FEDER', $words)  //strategy two 
+          //|| in_array('FEDERAL INLAND REVENUE SERV', $words)){   
             for ($x = 0; $x < count($words); $x++) {
                 $hays = $words[$x];
                 $myit = $x + 4;
@@ -218,34 +253,47 @@ function getFigures($str)
 
         // extract any debit entry(ies)
         $c = 1;
+        $acctNum=$extracts['accountNumber'];$lgt=strlen($acctNum)-3;
         for ($x = 0; $x < count($words); $x++) {
             $hays = $words[$x];
             $myit = $x + 5;
             // extract strings that have and find the word 'Multi Debit Entry' in it
-
             // if ((str_contains($words[$x], 'Multi Debit Entry') && substr_count($words[$x], ',') >= 1)) {
             if  (substr_count($hays, 'MDC2334800161') >= 1 
               || substr_count($hays, 'MDC2336300537') >= 1
               || substr_count($hays, 'MDC2335300162') >= 1
               || substr_count($hays, 'MDC2335500138') >= 1
               || substr_count($hays, 'MDC2335200043') >= 1
-              || substr_count($hays, 'MDC2401100156') >= 1) {
+              || substr_count($hays, 'MDC2401100156') >= 1
+              || substr_count($hays, 'MDC2335200062') >= 1) {
                 // extract the next strings after the above but has commas (figures) 
-                for ($u = $x + 1; $u <= $myit; $u++) {
-                    $e = $words[$u];
-                    if (substr_count($e, ',') >= 1) {
-                        $lbl = 'Debit-Entry-' . $c;
-                        // this is the correspnding figure for the Balance identified above
-                        $extracts[$lbl] = $words[$u];
-                        $c++;
-                        break;
+                if(substr($acctNum,$lgt,3) == '247' 
+                  || substr($acctNum,$lgt,3) == '032') { // 247 & 032 accts have a special treatment of debit entries
+                    $tmp=array_slice($words,$x,10);
+                    for ($u = 0; $u < count($tmp); $u++) {
+                        if (substr_count($tmp[$u], ',') >= 1) {
+                            $lbl = 'debitEntry-' . $c;
+                            // this is the correspnding figure for the Balance identified above
+                            $extracts[$lbl] = $tmp[$u];
+                            break 2;
+                        }
+                    }
+                } else {
+                    for ($u = $x + 1; $u <= $myit; $u++) {
+                        $e = $words[$u];
+                        if (substr_count($e, ',') >= 1) {
+                            $lbl = 'debitEntry-' . $c;
+                            // this is the correspnding figure for the Balance identified above
+                            $extracts[$lbl] = $words[$u];
+                            $c++;
+                            break;
+                        }
                     }
                 }
-                // }
             }
         }
     } else {
-        die('File does not content the appropriate dataset');
+        die('<b>File does not content the appropriate dataset</b>');
     }
 
     return $extracts;
@@ -317,6 +365,7 @@ function convertToNumber($str)
 
 function presentExtracts($arr)
 {
+    echo '<br>';
   foreach ($arr as $key => $val) {
     echo '<b>'. $key.'</b> => ' .$val.'<br>';
   }
